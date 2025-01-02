37008d27012de36f0781523d172f0fdcb189912c
ryodagg <mannai.abdlkader@gmail.com>
2025-01-02 17:48:50 +0100

    intro to factory

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,50 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'PHP Developer',
-                'salary' => '50000€'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Python Developer',
-                'salary' => '55000€'
-            ],
-            [
-                'id' => 3,
-                'title' => 'JavaScript Developer',
-                'salary' => '52000€'
-            ],
-            [
-                'id' => 4,
-                'title' => 'Java Developer',
-                'salary' => '58000€'
-            ],
-            [
-                'id' => 5,
-                'title' => 'Ruby Developer',
-                'salary' => '54000€'
-            ],
-        ];
-    }
+    use HasFactory;
+    protected $table = 'job_listings';
 
-    public static function find($id): array
-    {
-        // $jobs = static::all();
-        $jobs = self::all();
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = [
+        'title',
+        'salary',
+    ];
 }
