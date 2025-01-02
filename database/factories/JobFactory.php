49bf50ceb4abfeb76fc1583a2a7f558ec2ce4047
ryodagg <mannai.abdlkader@gmail.com>
2025-01-02 18:04:59 +0100

    relationships

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,8 +18,9 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
+            'employer_id' => Employer::factory(),
             'title' => fake()->jobTitle(),
-            'salary' => fake()->numberBetween(30, 100).',000€',
+            'salary' => fake()->numberBetween(30, 100) . ',000€',
         ];
     }
 }
