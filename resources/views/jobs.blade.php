61fad3408cc77012e43e44d364af3514fe478b04
ryodagg <mannai.abdlkader@gmail.com>
2025-01-03 14:05:53 +0100

    pagination

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,12 +5,16 @@
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach ($jobs as $job)
-            <a href="/job/{{ $job['id'] }}" class='border p-4 rounded-lg bg-gray-50 hover:shadow transition duration-300'>
+            <a href="/job/{{ $job['id'] }}"
+                class='border p-4 rounded-lg bg-gray-50 hover:shadow transition duration-300'>
                 <div class="text-sm text-blue-500 font-bold">{{ $job->employer->name }}</div>
                 <div class="text-gray-800">
                     <strong>{{ $job['title'] }}</strong> Pays {{ $job['salary'] }}
                 </div>
             </a>
         @endforeach
+    </div>
+    <div class="mt-4">
+        {{ $jobs->links() }}
     </div>
 </x-layout>
