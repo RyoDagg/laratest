8da9dc4898d4bd40cf10d844403923883118f6fe
ryodagg <mannai.abdlkader@gmail.com>
2025-01-03 13:15:24 +0100

    preventLazyLoading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,14 @@
         Job List
     </x-slot:heading>
 
-    <ul>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @foreach ($jobs as $job)
-            <li>
-              <a href="/job/{{ $job['id']}}" class='text-blue-500 hover:text-blue-600 underline'>
-                <strong>{{ $job['title'] }}</strong> Pays {{ $job['salary'] }}
+            <a href="/job/{{ $job['id'] }}" class='border p-4 rounded-lg bg-gray-50 hover:shadow transition duration-300'>
+                <div class="text-sm text-blue-500 font-bold">{{ $job->employer->name }}</div>
+                <div class="text-gray-800">
+                    <strong>{{ $job['title'] }}</strong> Pays {{ $job['salary'] }}
+                </div>
             </a>
-            </li>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
